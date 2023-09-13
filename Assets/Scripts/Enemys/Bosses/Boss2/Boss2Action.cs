0a6246be5d4456a1063fa1f3501c076010a92524
Geon Choi <131237383+geon3332@users.noreply.github.com>
2023-09-13 20:41:11 +0900

    23/09/13

--- Assets/Scripts/Enemys/Bosses/Boss2/Boss2Action.cs
+++ Assets/Scripts/Enemys/Bosses/Boss2/Boss2Action.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8bd5b875df7dfa88393aa54ae3d736be14c01220f9a0e713e1ee3c5f7799480
-size 21584
+oid sha256:63f1053bcb7c4d769b8e2ebd02407609b48ea436375cf336c0eacb709bf46e94
+size 20787
--- Assets/Scripts/Enemys/Bosses/Boss2/Boss2StageObjects.cs
+++ Assets/Scripts/Enemys/Bosses/Boss2/Boss2StageObjects.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e01bf9d2059f0abac97f97fd59b264886f690606708fad1106ff1c7f9bbb2b2
-size 9607
+oid sha256:39a587e537a6592e0d63a54336ee6f4ff48b7a5dfc08bcab6f7b3829bf321175
+size 10106
